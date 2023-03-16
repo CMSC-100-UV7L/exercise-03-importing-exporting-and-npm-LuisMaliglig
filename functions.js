e36e39e76b516c20f22f12dc7de7b05a71da2ff5
LuisMaliglig <lcmaliglig@up.edu.ph>
2023-03-17 01:31:58 +0800

    feat: added to generateUniqueID() function

--- functions.js
+++ functions.js
@@ -2,10 +2,13 @@ const fs = require("fs");
 const validator = require("validator");
 const uuid = require("uuid");
 
-function generateUniqueID(){
-
+function generateUniqueID(firstName, lastName){
+    const letter = firstName.charAt(0).toLowerCase();
+    const name = lastName.toLowerCase();
+    const uniqueString = uuid.v4().split("-")[0];
+    return '${letter}${name}${uniqueString}';
 }
 
 function addAccount(){
-    
+
 }
