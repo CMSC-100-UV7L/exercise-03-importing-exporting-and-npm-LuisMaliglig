70223afa588c0859997f4404306fe803788a0570
LuisMaliglig <lcmaliglig@up.edu.ph>
2023-03-17 02:03:41 +0800

    docs: added documentation

--- functions.js
+++ functions.js
@@ -3,33 +3,34 @@ const validator = require("validator");
 const uuid = require("uuid");
 const { type } = require("os");
 
+//function for unique ID
 function generateUniqueID(firstName, lastName){
-    const letter = firstName.charAt(0).toLowerCase();
-    const name = lastName.toLowerCase();
-    const uniqueString = uuid.v4().split("-")[0];
+    const letter = firstName.charAt(0).toLowerCase(); //takes first letter and converts to lowercase
+    const name = lastName.toLowerCase(); //converts last name to lowercase
+    const uniqueString = uuid.v4().split("-")[0]; //generated uuid will be separated prior to first hyphen, thus giving 8 characters
     return '${letter}${name}${uniqueString}';
 }
 
 function addAccount(firstName, lastName, email, age){
-    if (!firstName || !lastName || !email || !age){
+    if (!firstName || !lastName || !email || !age){ //checks if any field is empty
         return "Please fill out all fields";
     }
-    if(typeof firstName !== "string" || typeof lastName !== "string" || typeof email !== "string" || typeof age !== "number"){
+    if(typeof firstName !== "string" || typeof lastName !== "string" || typeof email !== "string" || typeof age !== "number"){ //checks for validity of input
         return "Invalid field type(s)";
     }
-    if(firstName.length == 0 || lastName.length == 0){
+    if(firstName.length == 0 || lastName.length == 0){ //makes sure last name and first name are non-empty strings
         return "Name fields must not be empty";
     }
-    if(!validator.isEmail(email)){
+    if(!validator.isEmail(email)){ //validates email format
         return "Invalid email format";
     }
-    if(age<18){
+    if(age<18){ //checks if age is less than 18
         return "Age must be at least 18";
     }
 
     const id = generateUniqueID(firstName,lastName);
-    const user = '${firstName},${lastName},${email},${age},${id}';
-    return fs.appendFileSync("users.txt", user + "\n");
+    const user = '${firstName},${lastName},${email},${age},${id}'; //concatenates string
+    return fs.appendFileSync("users.txt", user + "\n"); //adds user data to users.txt
 }
 
-module.exports = {generateUniqueID, addAccount};
+module.exports = {generateUniqueID, addAccount}; //ensures functions can be called by other files
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 const{generateUniqueID, addAccount} = require("functions.js");
 
-console.log(addAccount("Alan","Turing","aturing@w3c.com",58));
-console.log(addAccount("","Turing","aturing@w3c.com",58));
-console.log(addAccount("Alan","","aturing@w3c.com",58));
-console.log(addAccount("Alan","Turing","email_sample",58));
-console.log(addAccount("Alan","Turing","aturing@w3c.com",16));
-console.log(addAccount("Alan","Turing","aturing@w3c.com",));
-console.log(addAccount("Tim","Berners-Lee","tim@w3c.com",25));
-console.log(addAccount("Ted","Nelson","ted.n@w3c.com",43));
+console.log(addAccount("Alan","Turing","aturing@w3c.com",58)); //valid test
+console.log(addAccount("","Turing","aturing@w3c.com",58)); //checks for empty first name
+console.log(addAccount("Alan","","aturing@w3c.com",58)); //checks for empty last name
+console.log(addAccount("Alan","Turing","email_sample",58)); //checks for invalid email format
+console.log(addAccount("Alan","Turing","aturing@w3c.com",16)); //checks for age less than 18
+console.log(addAccount("Alan","Turing","aturing@w3c.com",)); //checks for empty age field
+console.log(addAccount("Tim","Berners-Lee","tim@w3c.com",25)); //valid test
+console.log(addAccount("Ted","Nelson","ted.n@w3c.com",43)); //valid test
